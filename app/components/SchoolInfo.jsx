a87416f09e29981e611b3cb8c05bfe839261f737
Beth Gilomen <gilomen2@gmail.com>
2016-10-13 18:55:26 -0500

    finish up part two

--- app/components/SchoolInfo.jsx
+++ app/components/SchoolInfo.jsx
@@ -1,11 +1,17 @@
 var React = require('react');
+var actions = require('../actions/SchoolActions');
 
 module.exports = React.createClass({
-  render:function(){
+  deleteSchool: function(e) {
+    e.preventDefault();
+    actions.deleteSchool(this.props.info);
+  },
+  render: function(){
     return (
       <div className="panel panel-default">
         <div className="panel-heading">
           {this.props.info.name}
+          <span className="pull-right text-uppercase delete-button" onClick={this.deleteSchool}>&times;</span>
         </div>
         <div className="panel-body">
           {this.props.info.tagline}
--- app/main.jsx
+++ app/main.jsx
@@ -3,12 +3,14 @@ var ReactDOM = require('react-dom');
 var SchoolsList = require('./components/SchoolsList.jsx');
 var schoolsStore = require('./stores/schoolsStore');
 
-var _schools = schoolsStore.getSchools();
+var _schools = [];
 
-schoolsStore.onChange(function(schools) {
-  _schools = schools;
-  render();
-});
+var getSchoolsCallback = function(schools) {
+    _schools = schools;
+    render()
+}
+
+schoolsStore.onChange(getSchoolsCallback);
 
 function render() {
   ReactDOM.render(<SchoolsList schools={_schools} />, document.getElementById('container'));
